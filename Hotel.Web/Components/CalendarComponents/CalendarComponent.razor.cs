c1388b2edf0b5396cd8be57cc1fb0ffa9365f70d
Fabian <grochla95@gmail.com>
2020-12-21 00:04:35 +0100

    add calendar component

---
+++ Hotel.Web/Components/CalendarComponents/CalendarComponent.razor.cs
@@ -0,0 +1,68 @@
+﻿using Hotel.Domain.Adapters;
+using Hotel.Domain.Environment;
+using Hotel.Web.Dtos;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+using Radzen.Blazor;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Web.Components.CalendarComponents
+{
+    public partial class CalendarComponent
+    {
+        [Inject] IReservationDao ReservationDao { get; set; }
+
+        private List<ReservationDto> _reservationsDtos;
+        private RadzenScheduler<ReservationDto> _scheduler;
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                var basicReservationsInfo = await ReservationDao.GetReservationBasicInfosAsync();
+
+                _reservationsDtos = basicReservationsInfo.Select(x => new ReservationDto
+                {
+                    Id = x.ReservationId,
+                    CheckIn = x.CheckIn,
+                    CheckOut = x.CheckOut.AddHours(Config.Get.FreeRoomHour),
+                    Text = $"Rezerwacja {x.ReservationId} - {x.Customer}"
+                }).ToList();
+
+                RefillColors();
+            }
+            catch (Exception ex)
+            {
+                await HandleException(ex);
+            }
+        }
+
+        private void RefillColors()
+        {
+            //var colors = Enum.GetValues(typeof(KnownColor)).Cast<KnownColor>().Where(x => !x.ToString().Contains("Light")).ToList();
+            var colors = new List<KnownColor>
+            {
+                KnownColor.Blue,
+                KnownColor.Green,
+                KnownColor.Purple,
+                KnownColor.Violet,
+                KnownColor.Orange,
+                KnownColor.Gray,
+                KnownColor.Cyan
+            };
+
+            var random = new Random();
+
+            _reservationsDtos.ForEach(x => x.Color = colors[random.Next(colors.Count())]);
+        }
+
+        private void OnSchedulerRender(SchedulerAppointmentRenderEventArgs<ReservationDto> args)
+        {            
+            args.Attributes["style"] = $"background: {args.Data.Color}";
+        }
+    }
+}
---
+++ Hotel.Web/Dtos/ReservationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+
+namespace Hotel.Web.Dtos
+{
+    public class ReservationDto
+    {
+        public int Id { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public string Text { get; set; }
+        public KnownColor Color { get; set; }
+    }
+}
