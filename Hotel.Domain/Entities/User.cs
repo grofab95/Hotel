d6451b5676d2e05c80b91e1bbfd63e10ca4578d5
Fabian <grochla95@gmail.com>
2021-05-05 00:51:07 +0200

    add user, user tests, password hash

---
+++ Hotel.Domain/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using Hotel.Domain.Entities.Common;
+using Hotel.Domain.Utilities.Models;
+using Hotel.Domain.Validators;
+
+namespace Hotel.Domain.Entities
+{
+    public class User : Entity
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public byte[] PasswordHash { get; private set; }
+        public byte[] PasswordSalt { get; private set; }
+
+        public User(string name, string email, Password password)
+        {
+            UserValidators.ValidIfNameExist(name);
+            UserValidators.ValidIfEmailExist(email);
+            UserValidators.ValidIfPasswordExist(password);
+            EmailValidators.ValidEmail(email);
+            Name = name;
+            Email = email;
+            PasswordHash = password.PasswordHash;
+            PasswordSalt = password.PasswordSalt;
+        }
+
+        public bool IsPasswordValid(string password)
+        {
+            return new Password(PasswordHash, PasswordSalt).Equals(password);
+        }
+    }
+}
---
+++ Hotel.Domain/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Hotel.Domain.Exceptions
+{
+    public class InvalidEmailException : Exception
+    {
+        public InvalidEmailException(string message) : base(message)
+        { }
+    }
+}
